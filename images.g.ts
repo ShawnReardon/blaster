0524595473907155fbb1da9d155fdbea6fdfc38e
ShawnReardon <shawnreardon1@gmail.com>
2021-07-11 18:51:35 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Y|_0Pb}kO{ZhDD0TiYi8":
+            case "princess":return img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . f 5 5 5 f . . . . . . 
+. . . . f 6 2 5 5 6 f . . . . . 
+. . . f 6 6 6 6 1 6 6 f . . . . 
+. . . f 6 6 6 6 6 1 6 f . . . . 
+. . . f d f d 6 6 6 1 f . . . . 
+. . . f d f d 6 6 6 6 f f . . . 
+. . . f d 3 d d 6 6 6 f 6 f . . 
+. . . . f d d d f f 6 f f . . . 
+. . . . . f f 5 3 f 6 6 6 f . . 
+. . . . f 5 3 3 f f f f f . . . 
+. . . . f 3 3 f d f . . . . . . 
+. . . . . f 3 f d f . . . . . . 
+. . . . f 3 5 3 f d f . . . . . 
+. . . . f f 3 3 f f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`;
+            case "image1":
+            case "princessParry":return img`
+. . 5 5 . . 5 . 5 . . . . . . . 
+. 5 5 . . f 5 5 5 f . . . . . . 
+5 5 5 . f 6 2 5 5 6 f . . . . . 
+5 5 5 f 6 6 6 6 1 6 6 f . . . . 
+5 5 5 f 6 6 6 6 6 1 6 f . . . . 
+5 5 5 f d f d 6 6 6 1 f . . . . 
+5 5 5 f d f d 6 6 6 6 f f . . . 
+5 5 5 f d 3 d d 6 6 6 f 6 f . . 
+5 5 5 . f d d d f f 6 f f . . . 
+5 5 5 . . f f 5 3 f 6 6 6 f . . 
+5 5 5 . f 5 3 3 f f f f f . . . 
+5 5 5 . f 3 3 f d f . . . . . . 
+5 5 5 . . f 3 f d f . . . . . . 
+. 5 5 . f 3 5 3 f d f . . . . . 
+. 5 5 . f f 3 3 f f . . . . . . 
+. . 5 5 . . f f f . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
